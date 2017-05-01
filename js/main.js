ca4efcff0236b3094d986dd73deb44408ea7c77d
Santosh <santushet@gmail.com>
2017-05-01 06:03:15 +0530

    initial commit

--- js/main.js
+++ js/main.js
@@ -148,11 +148,12 @@ app.config(['$routeProvider', function ($routeProvider) {
 			templateUrl: "partials/career.html"
 		})
 		.when("/contact", {
-			templateUrl: "partials/contact.html"
+			templateUrl: "partials/contact.html",
+			controller: "PageController"
 		})
-    .when("/solutions", {
-      templateUrl: "partials/solutions.html"
-    })
+		.when("/solutions", {
+			templateUrl: "partials/solutions.html"
+		})
 		.when("/solutions-1", {
 			templateUrl: "partials/solutions-1.html"
 		})
@@ -200,7 +201,7 @@ app.config(['$routeProvider', function ($routeProvider) {
 
 }]);
 
-app.controller('PageController', function () {
+app.controller('PageController', function ($scope) {
 	console.log("page controller");
 
 	$(".Modern-Slider").slick({
@@ -317,4 +318,80 @@ app.controller('PageController', function () {
 		animateOut: 'fadeOut',
 		animateIn: 'fadeIn'
 	});
+
+	//map
+
+
+	var newyork = new google.maps.LatLng(40.712784, -74.005941);
+
+	var style = [
+		{
+			"featureType": "road",
+			"elementType": "labels.icon",
+			"stylers": [
+				{
+					"saturation": 1
+						},
+				{
+					"gamma": 1
+						},
+				{
+					"visibility": "off"
+						},
+				 ]
+			},
+		{
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"saturation": -100
+						}
+				]
+			}
+		];
+
+	var mapOptions = {
+		// SET THE CENTER
+		center: newyork,
+
+		// SET THE MAP STYLE & ZOOM LEVEL
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		zoom: 12,
+
+		// SET THE BACKGROUND COLOUR
+		backgroundColor: "#eeeeee",
+
+		// REMOVE ALL THE CONTROLS EXCEPT ZOOM
+		panControl: false,
+		zoomControl: true,
+		mapTypeControl: false,
+		scaleControl: false,
+		scrollwheel: false,
+		streetViewControl: false,
+		overviewMapControl: false,
+		zoomControlOptions: {
+			style: google.maps.ZoomControlStyle.SMALL
+		}
+
+	}
+	$scope.map = new google.maps.Map(document.getElementById('googleMap2'), mapOptions);
+
+	// SET THE MAP TYPE
+	var mapType = new google.maps.StyledMapType(style, {
+		name: "Grayscale"
+	});
+	$scope.map.mapTypes.set('grey', mapType);
+	$scope.map.setMapTypeId('grey');
+
+	//CREATE A CUSTOM PIN ICON
+	var marker_image = 'images/pin.png';
+	var pinIcon = new google.maps.MarkerImage(marker_image, null, null, null, new google.maps.Size(21, 34));
+
+	marker = new google.maps.Marker({
+		position: newyork,
+		map: $scope.map,
+		icon: 'img/map-marker.png',
+		title: 'New York'
+	});
+
 });
